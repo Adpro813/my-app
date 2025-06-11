c61cb3a94cebf694e3002e0dce46c40bbb766c36
Joshua <cao.joshua232@gmail.com>
2025-06-11 00:24:52 -0700

    Fixing up loading screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "./HomeScreen.js";
+import LoadingScreen from "./screens/LoadingScreen.js"
 
 export default function App() {
   const Stack = createNativeStackNavigator();
@@ -11,7 +12,7 @@ export default function App() {
       <Stack.Navigator>
         <Stack.Screen name="Loading" component={LoadingScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Task" component={TaskScreen} />
+        {/* <Stack.Screen name="Task" component={TaskScreen} /> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
